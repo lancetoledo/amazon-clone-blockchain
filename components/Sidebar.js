9c788a498f68d091ae54bd6afe3a3b5555168ced
Lance Toledo <lanceatoledo@gmail.com>
2022-04-01 16:12:14 -0400

    UPDATE: Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { ConnectButton } from 'web3uikit'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+        <ConnectButton />
+    </div>
   )
 }
 
