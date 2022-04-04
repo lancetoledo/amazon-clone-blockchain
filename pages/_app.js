8f96b045201274f6f5eeb8c4760c688ab12e9ac6
Lance Toledo <lanceatoledo@gmail.com>
2022-04-04 18:54:11 -0400

    Updated build

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,8 @@ import { AmazonProvider } from '../context/AmazonContext'
 function MyApp({ Component, pageProps }) {
   return (
   <MoralisProvider
-  // serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}
-  // appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
-  serverUrl='https://xhhbk3phcxjf.usemoralis.com:2053/server'
-  appId='URSXuImCYdCqvBisbkl3CU1E938HS1PVFr9Q9bia'
+  serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}
+  appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
   >
     <AmazonProvider>
       <Component {...pageProps} />
